a13b92b89619071df98c94eec4dabe5c2ec87ff7
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-23 11:23:37 +0400

    Fix some notices

--- password_strength.py
+++ password_strength.py
@@ -14,36 +14,39 @@ def has_perfect_length(password):
 
 
 def contains_number(password):
-    return re.search(r"\d", password) is not None
+    return bool(re.search(r"\d", password))
 
 
 def contains_lowercase_letters(password):
-    return re.search(r"[a-z]", password) is not None
+    return bool(re.search(r"[a-z]", password))
 
 
 def contains_uppercase_letters(password):
-    return re.search(r"[A-Z]", password) is not None
+    return bool(re.search(r"[A-Z]", password))
 
 
 def contains_non_alphanumeric(password):
-    return re.search(r"[^a-zA-Z0-9]", password) is not None
+    return bool(re.search(r"[^a-zA-Z0-9]", password))
 
 
-def has_been_pwned(password):
+def get_pwnedpasswords_range(range):
     # https://haveibeenpwned.com/API/v2#PwnedPasswords
-    password_hash = hashlib.sha1(password.encode('utf-8')).hexdigest()
-    response_marker_to_search = password_hash[5:].upper()
-    api_param = password_hash[:5]
-    api_url = "https://api.pwnedpasswords.com/range/{}".format(api_param)
+    api_url = "https://api.pwnedpasswords.com/range/{}".format(range)
     api_headers = {'User-Agent': 'Pwnage-Checker-For-Devman'}
     try:
         with urllib.request.urlopen(
-                urllib.request.Request(api_url, headers=api_headers)) as f:
-            response = str(f.read())
+                urllib.request.Request(api_url, headers=api_headers)) as pointer:
+            return str(pointer.read())
     except urllib.error.URLError:
-        response = ''
+        return ''
 
-    return response.find(response_marker_to_search) > -1
+
+def has_been_pwned(password):
+    password_hash = hashlib.sha1(password.encode('utf-8')).hexdigest()
+    response_marker_to_search = password_hash[5:].upper()
+    range = password_hash[:5]
+
+    return get_pwnedpasswords_range(range).find(response_marker_to_search) > -1
 
 
 def is_diverse(password):
@@ -65,20 +68,21 @@ def matched_by_banned_masks(password):
 
 
 def get_password_strength(password):
-    strength = 1
-    strength += has_normal_length(password)
-    strength += has_perfect_length(password)
-    strength += contains_number(password)
-    strength += contains_lowercase_letters(password)
-    strength += contains_uppercase_letters(password)
-    strength += contains_non_alphanumeric(password)
-    strength += not has_been_pwned(password)
-    strength += is_diverse(password)
-    strength += not matched_by_banned_masks(password)
-    return strength
+    return sum([
+        1,
+        has_normal_length(password),
+        has_perfect_length(password),
+        contains_number(password),
+        contains_lowercase_letters(password),
+        contains_uppercase_letters(password),
+        contains_non_alphanumeric(password),
+        not has_been_pwned(password),
+        is_diverse(password),
+        not matched_by_banned_masks(password)
+    ])
 
 
 if __name__ == '__main__':
     password = getpass.getpass()
     strength = get_password_strength(password)
-    print('Your password\'s score is {}'.format(strength))
+    print("Your password's score is {}".format(strength))
